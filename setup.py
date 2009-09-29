ad4c8862c6c516049b9418fd5f2c35740528d99f
Thomas Lotze <tl@gocept.com>
2009-09-29 15:33:36 +0000

    Preparing release 3.7.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.site',
-      version = '3.7.0dev',
+      version = '3.7.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Local registries for zope component architecture',
@@ -68,7 +68,7 @@ setup(name='zope.site',
           'zope.event',
           'zope.interface',
           'zope.lifecycleevent',
-          'zope.location>=3.7.0dev',
+          'zope.location>=3.7.0',
           ],
       include_package_data = True,
       zip_safe = False,
