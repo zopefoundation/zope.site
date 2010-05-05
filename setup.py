79eee953a38fa7c097ea335bf19bba03aeecceb3
Tres Seaver <tseaver@palladion.com>
2010-05-05 19:28:17 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -28,7 +28,7 @@ def read(*rnames):
 
 setup(name='zope.site',
       version = '3.9.2dev',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Local registries for zope component architecture',
       long_description=(
