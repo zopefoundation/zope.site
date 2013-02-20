b599323db29a9cb0dbf91ad6cf19205ab75a78c7
Stephan Richter <stephan.richter@gmail.com>
2013-02-20 21:43:32 +0000

    Back to development: 4.0.0a2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.site',
-      version='4.0.0a1',
+      version='4.0.0a2.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Local registries for zope component architecture',
