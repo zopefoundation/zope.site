4815f6fe9b15970e288f98ac7accd481b8613d86
Michael Howitz <mh@gocept.com>
2020-09-09 16:50:12 +0200

    Adjust to change suggested by @jamadden.

--- src/zope/site/site.py
+++ src/zope/site/site.py
@@ -242,20 +242,19 @@ def SiteManagerAdapter(ob):
 
 def changeSiteConfigurationAfterMove(site, event):
     """
-    After a site is moved, its site manager links have to be
+    After a site is (re-)moved, its site manager links have to be
     updated.
 
     Subscriber to :class:`~.ISite` objects in a :class:`~.IObjectMovedEvent`.
     """
-    next = _findNextSiteManager(site)
     local_sm = site.getSiteManager()
     if event.newParent is not None:
+        next = _findNextSiteManager(site)
         if next is None:
             next = zope.component.getGlobalSiteManager()
         local_sm.__bases__ = (next, )
     else:
-        if next is not None:
-            next.removeSub(local_sm)
+        local_sm.__bases__ = ()
 
 
 @zope.component.adapter(
