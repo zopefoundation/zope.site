7a0414c506922493212d7f6bc92c9a7cdfb48959
Michael Howitz <mh@gocept.com>
2020-09-10 09:23:11 +0200

    Bumped version for feature release.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.site',
-      version='3.9.3.dev0',
+      version='3.10.0.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Local registries for zope component architecture',
